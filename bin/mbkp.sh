8ce503db558fb99dd4bdd704405969c7923865fe
Elifarley Callado Coelho Cruz <elifarley@gmail.com>
2011-01-26 09:17:41 -0200

    New module handling.

--- bin/mbkp.sh
+++ bin/mbkp.sh
@@ -1,35 +1,55 @@
-#!/bin/sh
-echo Testing count
-echo Count is "$#"
+#!/bin/bash
 [ "$#" -lt 1 ] && {
   echo "Usage:"
   echo "$0 <module-name-1> [<module-name-2>] ..."
   exit 1
 }
 
-BASEDIR="$(dirname $(readlink $0))"
+VERBOSE=0
 
-. "$BASEDIR/../mbackup-config.sh"
-echo "mbackup-config.sh called."
-
-mbkp_priv_hook="$BASEDIR/../priv/_mbackup.priv"
-[ -f "$mbkp_priv_hook" ] && {
-  . "$mbkp_priv_hook"
-  echo "$mbkp_priv_hook called."
-} || {
-  echo "Install $mbkp_priv_hook"
-  exit 1
+db_dump () {
+  # db_dump dump_name
+  local dump_name="$1"; shift
+  mysqldump "$@" -h "$DB_HOST" -u "$DB_USER" --password="$DB_PASSWORD" "$DB_NAME" > "$_module_cache/$dump_name.sql" || exit $?
 }
+
+dupl() {
+  PASSPHRASE=$PASSPHRASE duplicity -v4 \
+  --archive-dir="$HOME/.mbackup/cache/duplicity-archive" \
+  --name "$module" "$@"
+}
+
+do_backup() {
+  # do_backup src target
+
+  dupl cleanup "$2"
+  dupl remove-older-than 6M "$2"
+
+  # FTP_PASSWORD=$FTP_PASSWORD
+  dupl ${VOLSIZE:+--volsize="$VOLSIZE"} \
+  --ssh-askpass --exclude-other-filesystems \
+  $SSH_OPTS \
+  --full-if-older-than $full_if_older_than \
+  $includes $excludes \
+  $extra "$1/" "$2" || FAILED="$module"
+}
+
+BASEDIR="$(readlink $0)" || BASEDIR="$0"
+BASEDIR="$(dirname $BASEDIR)"
 
 module="$1"; shift
 
-priv_hook="$BASEDIR/../priv/$module.priv"
-[ -f "$priv_hook" ] && {
-  . "$priv_hook"
-  echo "$priv_hook called."
-}
+. "$BASEDIR/../mbackup-config.sh" || exit 1
+((VERBOSE)) && echo "mbackup-config.sh called."
 
-DUPLICITY="duplicity"
+mbkp_priv_hook="$BASEDIR/../priv/_mbackup.priv"
+if [ -f "$mbkp_priv_hook" ]; then
+  . "$mbkp_priv_hook" || exit 1
+  ((VERBOSE)) && echo "$mbkp_priv_hook called."
+else
+  echo "Please install $mbkp_priv_hook"
+  exit 1
+fi
 
 [ -d "$SYNC_ROOT" ] || {
   mkdir -p "$SYNC_ROOT" || exit $?
@@ -48,25 +68,39 @@ echo "=================================================="
 
 FAILED=''
 
+# Variables that can be overriden on module config files
 _module_cache="$SYNC_ROOT/$module"
+mbkp_src="$_module_cache"
 mbkp_target="$module"
 VOLSIZE="50"
+full_if_older_than="6M"
+extra=''
+
+priv_hook="$BASEDIR/../priv/$module.priv"
+[ -f "$priv_hook" ] && {
+  . "$priv_hook"
+  ((VERBOSE)) && echo "$priv_hook called."
+}
 
 [ -e "$_module_cache" ] || {
   mkdir "$_module_cache" || exit $?
 }
 
 pre_hook="$BASEDIR/../modules/$module.pre"
-[ -x "$pre_hook" ] && {
-  time $pre_hook "$_module_cache" || exit $?
-  echo "$pre_hook called."
-}
+if [ -s "$pre_hook" ]; then
+  time . $pre_hook "$_module_cache" || exit $?
+  ((VERBOSE)) && echo "$pre_hook called."
+else
+  echo "### $pre_hook NOT CALLED."
+fi
 
-echo "Module cache at $_module_cache"
+mbkp_full_target="$MBKP_BASE_TARGET/$mbkp_target"
+
 echo "Volume size: $VOLSIZE MB"
-
-. "$BASEDIR/../modules/$module.sh" "$_module_cache"
-echo "$module.sh called."
+echo "Module source at $mbkp_src"
+echo "Module target at $mbkp_full_target"
+echo "full_if_older_than: $full_if_older_than"
+echo "extra: $extra"
 
 includes="$BASEDIR/../modules/$module.includes"
 [ -s "$includes" ] && {
@@ -78,21 +112,15 @@ excludes="$BASEDIR/../modules/$module.excludes"
   excludes="--exclude-globbing-filelist $excludes"
 } || excludes=''
 
-
-PASSPHRASE=$PASSPHRASE $DUPLICITY -v4 \
---name "$module" --volsize="$VOLSIZE" \
---archive-dir=$HOME/.mbackup/cache/duplicity-archive \
---exclude-other-filesystems \
-$SSH_OPTS \
---full-if-older-than $full_if_older_than \
-$includes $excludes \
-$extra "$mbkp_src/" "$MBKP_BASE_TARGET/$mbkp_target" || FAILED="$module"
+do_backup "$mbkp_src/" "$mbkp_full_target"
 
 post_hook="$BASEDIR/../modules/$module.post"
-[ -x "$post_hook" ] && {
-  $post_hook "$_module_cache" || FAILED="$post_hook"
-  echo "$post_hook called."
-}
+if [ -s "$post_hook" ]; then
+ . $post_hook "$_module_cache" || FAILED="$post_hook"
+  ((VERBOSE)) && echo "$post_hook called."
+else
+  echo "### $post_hook NOT CALLED."
+fi
 
 [ -z "$FAILED" ] || {
   echo "##################################################"
