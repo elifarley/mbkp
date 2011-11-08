dc48ef51b3cb192a67cbd37882e17dc661199c6c
Elifarley Callado Coelho Cruz <elifarley@gmail.com>
2011-11-08 14:40:01 -0200

    Added status command

--- bin/mbkp-verify.sh
+++ bin/mbkp-verify.sh
@@ -7,4 +7,4 @@ module="$1"; shift
 init
 init_module
 
-echo "status:" && dupl -v8 --dry-run "${file_selection[@]}" "$@" "$mbkp_src" "$mbkp_full_target"
+echo "verify:" && dupl verify -v8 "${file_selection[@]}" "$@" "$mbkp_full_target" "$mbkp_src"
