f9aafffd875c1dc3599c79883d0c939c034b217a
Elifarley Callado Coelho Cruz <elifarley@gmail.com>
2011-11-16 12:34:16 -0200

    Improved help messages

--- inc/help.inc.sh
+++ inc/help.inc.sh
@@ -20,45 +20,59 @@ EXP_help() {
 }
 
 EXP_help_usage() {
-  echo "Usage:"
   echo "$0 help <topic>"
+  echo
+  echo "show help on a topic"
+  echo
   exit 1
 }
 
 EXP_backup_modules_usage() {
-  echo "Usage:"
   echo "$0 backup <module-1> [<module-2> ...]"
+  echo
+  echo "perform a backup"
+  echo
   exit 1
 }
 
 EXP_status_usage() {
-  echo "Usage:"
   echo "$0 status <module> [<param> ...]"
+  echo
+  echo "list changed files"
+  echo
   exit 1
 }
 
 EXP_list_usage() {
-  echo "Usage:"
   echo "$0 list [<module>] [<param> ...]"
+  echo
+  echo "list files contained in a backup"
+  echo
   exit 1
 }
 
 EXP_verify_usage() {
-  echo "Usage:"
   echo "$0 verify <module> [<param> ...]"
+  echo
+  echo "verify files contained in a backup against those at the source"
+  echo
   exit 1
 }
 
 EXP_restore_usage() {
-  echo "Usage:"
   echo "$0 restore <module> [<param> ...]"
+  echo
+  echo "restore files"
+  echo
   exit 1
 }
 
 EXP_call_usage() {
-  echo "Usage:"
   echo "$0 call <module> <hook-type>"
-  echo "<hook-type> can be 'pre' or 'post'"
+  echo
+  echo "call a pre- or post-backup hook"
+  echo
+  echo "A hook is executed before or after a backup is performed. <hook-type> can be 'pre' or 'post'"
   exit 1
 }
 
